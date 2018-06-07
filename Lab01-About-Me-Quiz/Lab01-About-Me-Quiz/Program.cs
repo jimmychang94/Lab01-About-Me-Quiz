615bfb07ef8aea4b39e81a50fb3e30f8e1ec7665
Jimmy Chang <jimmyfchang94@gmail.com>
2018-06-06 23:08:28 -0700

    Fixes code breaks and adds comments

--- Lab01-About-Me-Quiz/Lab01-About-Me-Quiz/Program.cs
+++ Lab01-About-Me-Quiz/Lab01-About-Me-Quiz/Program.cs
@@ -6,11 +6,14 @@ namespace Lab01_About_Me_Quiz
     {
         static void Main(string[] args)
         {
-            // The method Check is called here and the output of the method is assigned to the variable correct which is then used in the Console.WriteLine.
             byte correct = Check();
             Console.WriteLine($"You got {correct}/5 questions right!");
         }
 
+        /// <summary>
+        /// This method is used to play the quiz while also tallying up the number of correct answers.
+        /// </summary>
+        /// <returns>This method returns a byte of the number of correct answers that were given.</returns>
         static byte Check ()
         {
             byte correct = 0;
@@ -23,6 +26,11 @@ namespace Lab01_About_Me_Quiz
             return correct;
         }
 
+        /// <summary>
+        /// This method is used to set up a question which asks which state I was born in.
+        /// </summary>
+        /// <param name="correct">This byte tallies the number of correct answers the user has answered</param>
+        /// <returns>This method returns the tally of correct answers after determining if the user got this question right</returns>
         static byte CheckBirthState (byte correct)
         {
             string userGuessBirthState = BirthState();
@@ -36,6 +44,11 @@ namespace Lab01_About_Me_Quiz
             return correct;
         }
 
+        /// <summary>
+        /// This method is used to set up a question which asks a true or false question.
+        /// </summary>
+        /// <param name="correct">This byte tallies the number of correct answers the user has answered</param>
+        /// <returns>This method returns the tally of correct answers after determining if the user got this question right</returns>
         static byte CheckGameConsoles(byte correct)
         {
             bool userGuessGameConsoles = GameConsoles();
@@ -51,6 +64,11 @@ namespace Lab01_About_Me_Quiz
             return correct;
         }
 
+        /// <summary>
+        /// This method is used to set up a question which asks what my favorite number is.
+        /// </summary>
+        /// <param name="correct">This byte tallies the number of correct answers the user has answered</param>
+        /// <returns>This method returns the tally of correct answers after determining if the user got this question right</returns>
         static byte CheckFavoriteNumber(byte correct)
         {
             int userGuessFavoriteNumber = FavoriteNumber();
@@ -63,6 +81,11 @@ namespace Lab01_About_Me_Quiz
             return correct;
         }
 
+        /// <summary>
+        /// This method is used to set up a question which asks which movie series I have not seen.
+        /// </summary>
+        /// <param name="correct">This byte tallies the number of correct answers the user has answered</param>
+        /// <returns>This method returns the tally of correct answers after determining if the user got this question right</returns>
         static byte CheckMovies(byte correct)
         {
             byte userGuessMovie = Movies();
@@ -76,6 +99,11 @@ namespace Lab01_About_Me_Quiz
             return correct;
         }
 
+        /// <summary>
+        /// This method is used to set up a question which asks what my favorite fruit is.
+        /// </summary>
+        /// <param name="correct">This byte tallies the number of correct answers the user has answered</param>
+        /// <returns>This method returns the tally of correct answers after determining if the user got this question right</returns>
         static byte CheckFavoriteFruit(byte correct)
         {
             // Here we have the built-in string method ToLower() so that we don't need to test for cases when the user capitalizes the first letter.
@@ -91,39 +119,59 @@ namespace Lab01_About_Me_Quiz
         }
 
         // The below methods all pose a question to the user and returns the response the user types in (this is used for checking to see if the user input the right ansswer).
+        /// <summary>
+        /// This method poses a question "What state was I born in?"
+        /// </summary>
+        /// <returns>This method returns the user's response.</returns>
         static string BirthState ()
         {
             Console.WriteLine("What state was I born in?");
             return Console.ReadLine();
         }
 
+        /// <summary>
+        /// This method poses a question "True or False? I have a Nintendo 64, Wii, and Nintendo Switch in my house."
+        /// </summary>
+        /// <returns>This method returns the user's response after verifying if it is a valid input.</returns>
         static bool GameConsoles ()
         {
             Console.WriteLine("True or False? I have a Nintendo 64, Wii, and Nintendo Switch in my house.");
             // Since the Console.ReadLine() takes the user input and returns it as a string; 
             // I need to convert the variable type to boolean since that is the type that is returned from the method.
             // This same idea was applied for any other variable type besides string
-            bool userGuessGameConsoles = bool.Parse(Console.ReadLine());
+            Boolean.TryParse(Console.ReadLine(), out bool userGuessGameConsoles);
             return userGuessGameConsoles;
         }
 
+        /// <summary>
+        /// This method poses a question "What is my favorite number? It is between 0 and 100."
+        /// </summary>
+        /// <returns>This method returns the user's response after verifying if it is a valid input.</returns>
         static int FavoriteNumber ()
         {
             Console.WriteLine("What is my favorite number? It is between 0 and 100.");
-            int userGuessFavoriteNumber = int.Parse(Console.ReadLine());
+            Int32.TryParse(Console.ReadLine(), out int userGuessFavoriteNumber);
             return userGuessFavoriteNumber;
         }
 
+        /// <summary>
+        /// This method poses a question "Which of these movie series have I not seen?"
+        /// </summary>
+        /// <returns>This method returns the user's response after verifying if it is a valid input.</returns>
         static byte Movies ()
         {
             Console.WriteLine("Which of these movie series have I not seen?");
             Console.WriteLine("1. Star Wars");
             Console.WriteLine("2. Harry Potter");
             Console.WriteLine("3. The Lord of the Rings");
-            byte userGuessMovie = byte.Parse(Console.ReadLine());
+            Byte.TryParse(Console.ReadLine(), out byte userGuessMovie);
             return userGuessMovie;
         }
 
+        /// <summary>
+        /// This method poses a question "what is my favorite fruit?"
+        /// </summary>
+        /// <returns>This method returns the user's response.</returns>
         static string FavoriteFruit ()
         {
             Console.WriteLine("What is my favorite fruit?");
